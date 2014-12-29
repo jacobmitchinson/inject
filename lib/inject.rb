dc4f71cf78f48ec0883fadca29bfe2f024de487e
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-29 18:21:16 +0000

    getting there with symbol usage but still getting no block given

--- lib/inject.rb
+++ lib/inject.rb
@@ -2,6 +2,7 @@ class Array
 
   def jake_inject(argument = false, symbol = false)
     copy = self.dup 
+    symbol = argument && argument = false if argument.is_a? Symbol
     return jake_inject(argument, &symbol) if symbol
     argument ? memo = argument : memo = copy[0] && copy.delete_at(0)
     copy.each { |item| memo = yield memo, item }
