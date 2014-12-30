80cdf5dafae6e20c2c71508a2bc8144ddc625fac
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-30 20:37:14 +0000

    completed inject with reactored version. YAY!

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,11 +1,10 @@
-class Array 
+class Array
 
-  def jake_inject(argument = false, symbol = false)
-    copy = self.dup 
-    symbol = argument && argument = false if argument.is_a? Symbol
+  def jake_inject(argument = false, symbol = false, &block)
+    return jake_inject(&argument) if argument.is_a? Symbol
     return jake_inject(argument, &symbol) if symbol
-    argument ? memo = argument : memo = copy[0] && copy.delete_at(0)
-    copy.each { |item| memo = yield memo, item }
+    argument ? memo = argument : memo = self[0] && self.delete_at(0)
+    self.each { |item| memo = block.call memo, item }
     return memo
   end
   
